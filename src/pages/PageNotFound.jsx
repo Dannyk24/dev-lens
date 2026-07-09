ec87f516c690c092b04738e9cbf6d036780fbb6f
Dannyk24 <teckydan@gmail.com>
2026-07-09 09:01:48 +0100

    create page not found component

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,19 @@
+import Navbar from "../components/Navbar";
+import { Link } from "react-router";
+import "./PageNotFound.css";
+
 function PageNotFound() {
-  return <div className="profile-page">Page Not Found </div>;
+  return (
+    <>
+      <Navbar />
+      <div className="profile-page">
+        <span>Page Not Found </span>
+        <Link className="cta primary-cta back-to-home-cta" to="/">
+          Back to Home
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default PageNotFound;
