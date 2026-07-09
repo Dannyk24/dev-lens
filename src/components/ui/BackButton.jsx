625dbd7bc2be6a86e6593bdfc4e629c8d5f0b142
Dannyk24 <teckydan@gmail.com>
2026-07-09 09:01:15 +0100

    create back button component and reduce body padding to 100px

---
+++ src/components/ui/BackButton.jsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router";
+import { ChevronLeft } from "lucide-react";
+import "./BackButton.css";
+
+function BackButton() {
+  const navigate = useNavigate();
+  return (
+    <div
+      className="back-button"
+      onClick={() => {
+        navigate(-1);
+      }}
+    >
+      <ChevronLeft />
+      <span>Back</span>
+    </div>
+  );
+}
+
+export default BackButton;
